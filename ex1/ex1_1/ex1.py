3ebb3692284322dee627c28788a78ad26217f3af
gongel <1324522527@qq.com>
2018-07-10 22:22:14 +0800

    ex1_1

--- ex1/ex1_1/ex1.py
+++ ex1/ex1_1/ex1.py
@@ -9,13 +9,13 @@ print(data.head())#显示数据前五行
 print(data.describe())#描述数据的数量，平均值，中位数等等
 
 
-def computerCost(X,y,theta):
+def computerCost(X,y,theta):# J(θ0,θ1)
     #     """
     #     X: R(m*n), m 样本数, n 特征数
     #     y: R(m)
     #     theta : R(n), 线性回归的参数
     #     """
-    inner = np.power((X * theta.T-y),2)
+    inner = np.power((X * theta.T-y),2) #矩阵中每个元素平方
     return np.sum(inner)/(2 * len(X))
 
 data.insert(0,'ones',1)
@@ -33,17 +33,20 @@ X = data.iloc[:,0:cols-1] #X是所有行，然后去掉最后一列
 y = data.iloc[:,cols-1:cols]#y是所有行，最后一列
 #iloc(行，列)
 
-print(X.head())
-print(y.head())
+# print(X.head())
+# print(y.head())
 
 X = np.matrix(X.values)
 y = np.matrix(y.values)
 theta = np.matrix(np.array([0,0]))#theta初始化为1x2的零矩阵
 
-print(theta)
-print(X.shape)
-print(theta.shape)
-print(y.shape)
+# print(theta)
+# print(X.shape)
+# print("x[:,1]:",X[:,1])
+# print(theta.shape)
+# print(y.shape)
+# print('.............................................')
+# print(y)
 
 print(computerCost(X,y,theta))
 print( np.matrix(np.zeros(theta.shape)).shape)
@@ -55,10 +58,11 @@ def gradientDescent(X,y,theta,alpha,iters):
     cost = np.zeros(iters)
 
     for i in range(iters):
-        error = (X * theta.T) - y
+        error = (X * theta.T) - y #此时erros为一个mx1的矩阵
+
         for j in range(parameters):
-            term = np.multiply(error,X[:,j])
-            temp[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term)) #theta第0行第0列或者第1列
+            term = np.multiply(error,X[:,j]) #矩阵对应位置元素相乘
+            temp[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term)) #theta第0行第0列或者第1列；sum求矩阵中所有元素的和
         theta = temp
         cost[i] = computerCost(X,y,theta)
     return theta,cost
